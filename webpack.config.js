73c13b9044b9b0a54ec49895f57c0c5e893776b5
Ok Kim <okkim21@gmail.com>
2017-05-11 20:37:00 -0700

    edit webpack config font

--- webpack.config.js
+++ webpack.config.js
@@ -47,9 +47,9 @@ module.exports = {
                     "url-loader?name=/images/[name].[hash:12].[ext]&limit=10000"
                 ]
             },
-            { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+            { test: /\.(woff2?|svg)$/, loader: 'file-loader?limit=10000&name=fonts/[name].[ext]' },
             { test: /\.(ttf|eot)$/, loader: 'file-loader?name=fonts/[name].[ext]' },
-            { test: /\.(mp4|webm|ogv)$/, loader: 'url-loader?name=images/[name].[ext]' }
+            { test: /\.(mp4|webm|ogv)$/, loader: 'file-loader?name=images/[name].[ext]' }
         ]
     },
     devServer: {
